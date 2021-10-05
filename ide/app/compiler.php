b42f538f2df1d0feee291661db5253c2e911f130
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2021-10-05 20:33:40 -0300

    compilador alterado para salvar os arquivos

--- ide/app/compiler.php
+++ ide/app/compiler.php
@@ -12,39 +12,27 @@ fclose($programFile);
 if ($language == "php") {
     $output = shell_exec("php $filePath 2>&1");
     echo $output;
-    if (file_exists($filePath)) {
-        unlink($filePath);
-    }
+   
 } else if ($language == "py") {
     $output = shell_exec("python $filePath 2>&1");
     echo $output;
-    if (file_exists($filePath)) {
-        unlink($filePath);
-    }
+    
 } else if ($language == "js") {
     $output = shell_exec("node $filePath 2>&1");
     echo $output;
-    if (file_exists($filePath)) {
-        unlink($filePath);
-    }
+    
 } else if ($language == "c") {
 
     $outputExe = "temp/" . $language . "/" . $random . ".exe";
     $error = shell_exec("gcc $filePath -o $outputExe");
     $output = shell_exec($outputExe);
     echo $output;
-    if (file_exists($filePath)) {
-        unlink($filePath);
-        unlink($outputExe)
-    }
+  
 } else if ($language == "cpp") {
 
     $outputExe = "temp/" . $language . "/" . $random . ".exe";
     shell_exec("g++ $filePath -o $outputExe");
     $output = shell_exec($outputExe);
     echo $output;
-    if (file_exists($filePath)) {
-        unlink($filePath);
-        unlink($outputExe)
-    }
+   
 }
