c2637655de90f4b41d910842478a6e9becb70a40
Matheus Tirabassi <64479165+matheustirabassi@users.noreply.github.com>
2022-08-18 14:50:35 -0300

    Cria entidade Question e Quizz, e as relaciona (#61)
    * Cria entidade Question e Quizz, e as relaciona
    
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
    
    * Corrige nome da coluna de resposta para a entidade Question
    
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
    
    * Incrementa versao para 1.2
    
    Signed-off-by: Matheus Henrique TIrabassi <tirabassi.matheus@aluno.ifsp.edu.br>
    
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
    Signed-off-by: Matheus Henrique TIrabassi <tirabassi.matheus@aluno.ifsp.edu.br>

---
+++ backend/src/main/kotlin/com/br/gglearning/domain/Question.kt
@@ -0,0 +1,37 @@
+package com.br.gglearning.domain
+
+import java.util.Date
+import javax.persistence.CollectionTable
+import javax.persistence.Column
+import javax.persistence.ElementCollection
+import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.JoinTable
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+/**
+ * A questão relacionada ao quizz.
+ *
+ * @property text A pergunta da questão.
+ * @property alternatives As possíveis escolhas de respostas.
+ * @property answer A resposta da pergunta.
+ * @property quizz O quizz da pergunta.
+ */
+@Entity
+@Table(name = "question")
+class Question (
+    @Column(name = "text")
+    var text: String,
+
+    @ElementCollection
+    @CollectionTable(name = "question_alternatives")
+    var alternatives: List<String>,
+
+    @Column(name = "answer")
+    var answer: String,
+
+    @ManyToOne
+    @JoinColumn(name = "quizz_id")
+    var quizz: Quizz
+): BaseEntity()
