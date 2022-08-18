b360caa4ac218269afd030865cf6d475dfac5a4b
Matheus Tirabassi <64479165+matheustirabassi@users.noreply.github.com>
2022-08-18 15:03:18 -0300

    Cria configuração de segurança com spring security (#62)
    * Cria configuração de segurança com spring security
    
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
    
    * Cria configuração de segurança com spring security
    
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
    
    * Incrementa versão para 1.3
    
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
    
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>

--- backend/src/main/kotlin/com/br/gglearning/controllers/UserController.kt
+++ backend/src/main/kotlin/com/br/gglearning/controllers/UserController.kt
@@ -4,6 +4,7 @@ import com.br.gglearning.data.UserDto
 import com.br.gglearning.services.UserService
 import lombok.extern.log4j.Log4j2
 import org.springframework.http.ResponseEntity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -23,7 +24,10 @@ class UserController(
         return ResponseEntity.ok(userService.findAllUsers())
     }
 
-    @PostMapping
+    /**
+     * Insere um novo usuário no sistema.
+     */
+    @PostMapping(value = ["/create"])
     fun insertNewUser(
         @Valid
         @RequestBody
