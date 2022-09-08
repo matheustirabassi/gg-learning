1efcd743962ef2ea897902085c09cbc740eca468
miguelsbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2022-09-08 12:36:22 -0300

    feat: :sparkles: Adiciona ViewModel para tela de criar conta
    78

---
+++ frontend/src/presentation/view/CreateAccountPage/CreateAccountContentViewModel.ts
@@ -0,0 +1,48 @@
+import * as yup from "yup"
+import { yupResolver } from '@hookform/resolvers/yup'
+import "../../../assets/yup/TraducoesYup"
+import { Api } from "presentation/axios/AxiosConfig"
+
+export interface ICreateAccountInput {
+    name: string
+    cpf: string
+    email: string
+    birthDate: string
+    typeUser: string
+    userName: string
+    password: string
+}
+
+export type UserType = { label: string, value: string }
+
+const createAccountSchema = yup.object().shape({
+    name: yup.string().required(),
+    cpf: yup.string().required(),
+    email: yup.string().email().required(),
+    birthDate: yup.string().required().min(10).max(10),
+    typeUser: yup.string().required(),
+    userName: yup.string().required(),
+    password: yup.string().required().min(3),
+})
+
+const createUser = async (user: ICreateAccountInput) => {
+        const data = await Api.post('/users/create', {
+            "name": user.name,
+            "cpf": user.cpf,
+            "email": user.email,
+            "typeUser": user.typeUser,
+            "userName": user.userName,
+            "password": user.password 
+        })
+        console.log(data)
+        if (data) {
+            console.log(data)
+        }else{
+            console.log(data)
+        }
+}
+
+export const CreateAccountViewModel = {
+    createUser,
+    createAccountSchema
+}
