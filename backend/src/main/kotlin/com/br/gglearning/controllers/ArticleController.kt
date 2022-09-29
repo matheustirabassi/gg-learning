2d0128208e897dc93550cdd4c62f63663ce49906
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2022-09-29 16:11:59 -0300

    Cria endpoint que busca todos os artigos
    Implementa #73.
    
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>

--- backend/src/main/kotlin/com/br/gglearning/controllers/ArticleController.kt
+++ backend/src/main/kotlin/com/br/gglearning/controllers/ArticleController.kt
@@ -4,8 +4,11 @@ import com.br.gglearning.data.ArticleDto
 import com.br.gglearning.services.ArticleService
 import lombok.extern.log4j.Log4j2
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.annotation.Secured
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -40,5 +43,13 @@ class ArticleController(
             .buildAndExpand(article.id).toUri()
 
         return ResponseEntity.created(uri).build()
+    }
+
+    /**
+     * Busca todos os artigos do sistema.
+     */
+    @GetMapping
+    fun findAllArticles(pageable: Pageable): ResponseEntity<Page<ArticleDto>> {
+        return ResponseEntity.ok().body(articleService.findAllArticles(pageable))
     }
 }
