12be0263d0fdadd29ea9e53b43eea58ff5e5edae
miguelsbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2022-10-10 20:20:35 -0300

    feat: :sparkles: Cria método para validar quizz

--- frontend/src/presentation/view/QuizzPage/QuizzContentView.tsx
+++ frontend/src/presentation/view/QuizzPage/QuizzContentView.tsx
@@ -6,15 +6,32 @@ import { Question } from "presentation/components/Question/Question"
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { ROUTES } from "Routes"
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from "yup"
+import "../../../assets/yup/TraducoesYup"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { RHRadioButton } from "presentation/components/FormComponents/RHRadioButton"
 
 interface IQuizzProps {
     id: number
 }
 
+const quizzSchema = yup.object().shape({
+    alternatives: yup.array().of(yup.string().required()),
+})
+
+interface IQuizzAlternatives {
+    alternatives: string[]
+}
+
 export const QuizzContentView = ({ id }: IQuizzProps) => {
     const [isLoading, setIsLoading] = useState(false)
-    const [article, setArticle] = useState<ArticleDTO>(new ArticleDTO('','','','','',[]))
+    const [article, setArticle] = useState<ArticleDTO>(new ArticleDTO('', '', '', '', '', []))
+    const [answers, setAnswers] = useState([""])
     const navigate = useNavigate()
+    const { control, handleSubmit, reset } = useForm<IQuizzAlternatives>({
+        resolver: yupResolver(quizzSchema)
+    })
 
     // useEffect(() => {
     //     setIsLoading(true)
@@ -32,28 +49,45 @@ export const QuizzContentView = ({ id }: IQuizzProps) => {
     //         })
 
     // }, [id])
+
+    const onSendQuizz: SubmitHandler<IQuizzAlternatives> = (data) => {
+        let count = 0
+        let total = 0
+        data.alternatives.forEach((ans, i) =>{
+            total += 1
+            if(ans == article.quizzes[0].questions[i].answer){
+                count += 1
+            }
+        })
+
+        alert(`Você acertou ${total}/${count}`)
+        reset()
+    }
     return (
         <PageBaseLayout>
             <Box display="flex" justifyContent="center" >
                 <FormControl sx={{ marginTop: "20px" }}>
                     <Grid container spacing={2} direction="column">
-                        {/* {article?.quizzes[0].questions.map((question, index) => {
+                        {article?.quizzes[0].questions.map((question, index) => {
                             return (
                                 <Grid item key={index}>
-                                    <Question
-                                        num={index}
-                                        question={question.text}
-                                        alternatives={question.alternatives}
-                                    />
+                                    {/* <RHRadioButton
+                                        control={control}
+                                        name={`alternatives[${index}]`}
+                                        label={question.text}
+                                        options={question.alternatives}
+                                        disabled={isLoading}
+                                    /> */}
                                 </Grid>
                             )
-                        })} */}
+                        })}
                         <Grid item >
                             <Question
                                 num={1}
                                 question="O python é fortemente tipado?"
                                 alternatives={["Sim", "Não"]}
                             />
+
                         </Grid>
                         <Grid item>
                             <Question
@@ -79,6 +113,7 @@ export const QuizzContentView = ({ id }: IQuizzProps) => {
                                 marginTop: "35px",
                                 marginBottom: "20px"
                             }}
+                            onClick={handleSubmit(onSendQuizz)}
                         >
                             <Typography variant="h3">
                                 Enviar Quizz
