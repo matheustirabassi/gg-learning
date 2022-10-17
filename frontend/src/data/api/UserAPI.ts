1bc355c6b69d38f622c86b1e087b189c51d1df0c
miguelsbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2022-10-17 20:14:52 -0300

    fix: :bug: Muda url do axios e tira import nao usado

--- frontend/src/data/api/UserAPI.ts
+++ frontend/src/data/api/UserAPI.ts
@@ -1,5 +1,4 @@
 import { Api } from "config/axios/AxiosConfig"
-import { useAuthContext } from "contexts/AuthContext"
 import { UserDTO } from "data/dto/UserDTO"
 const create = async (user: UserDTO) => {
     console.log(user)
@@ -9,7 +8,7 @@ const create = async (user: UserDTO) => {
         "email": user.email,
         "typeUser": user.typeUser,
         "userName": user.userName,
-        "birthDate":user.birthDate,
+        "birthDate": user.birthDate,
         "password": user.password
     })
     console.log(data)
@@ -17,16 +16,16 @@ const create = async (user: UserDTO) => {
 
 async function getMyAccountData() {
 
-	await Api.get('/users/myUser')
-	.then((res) => {
-		console.info(res.data as UserDTO)
-		return res
-	}).catch((error) => {
-		console.error(error)
-	})
+    await Api.get('/users/myUser')
+        .then((res) => {
+            console.info(res.data as UserDTO)
+            return res
+        }).catch((error) => {
+            console.error(error)
+        })
 }
 
 export const UserAPI = {
     create,
-		getMyAccountData
+    getMyAccountData
 }
