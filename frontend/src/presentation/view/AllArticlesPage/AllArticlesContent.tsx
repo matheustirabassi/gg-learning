ff5e41c36550cda353a92bc8d8e42cdade8093bf
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2022-12-06 15:21:50 -0300

    feat: :sparkles: Adiciona Skeleton para cards de Artigo

--- frontend/src/presentation/view/AllArticlesPage/AllArticlesContent.tsx
+++ frontend/src/presentation/view/AllArticlesPage/AllArticlesContent.tsx
@@ -1,4 +1,4 @@
-import { Grid, Box} from "@mui/material";
+import { Grid, Box, Skeleton } from "@mui/material";
 import { useAuthContext } from 'contexts/AuthContext';
 import { ArticleAPI } from 'data/api/ArticleAPI';
 import { ArticleDTO } from 'data/dto/ArticleDTO';
@@ -10,12 +10,15 @@ import { useEffect, useState } from 'react';
 export const AllArticlesContent = () => {
     const [articles, setArticle] = useState<ArticleDTO[]>([])
     const { debounce } = useDebounce(5000)
-    const { token } = useAuthContext() 
+    const { token } = useAuthContext()
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
+        setIsLoading(true)
         debounce(() => {
             ArticleAPI.getAll(token)
                 .then((result) => {
+                    setIsLoading(false)
                     if (result instanceof Error) {
                         alert(result.message)
                     } else {
@@ -46,6 +49,28 @@ export const AllArticlesContent = () => {
                                         </Grid>
                                     )
                                 })
+                            }
+
+                            {
+                                isLoading && (
+                                    <>
+                                        <Grid item xs={12} sm={6} lg={4} xl={3} marginBottom={2}>
+                                            <Skeleton variant="rectangular" height={"400px"} width="300px" />
+                                        </Grid>
+                                        <Grid item xs={12} sm={6} lg={4} xl={3} marginBottom={2}>
+                                            <Skeleton variant="rectangular" height={"400px"} width="300px" />
+                                        </Grid>
+                                        <Grid item xs={12} sm={6} lg={4} xl={3} marginBottom={2}>
+                                            <Skeleton variant="rectangular" height={"400px"} width="300px" />
+                                        </Grid>
+                                        <Grid item xs={12} sm={6} lg={4} xl={3} marginBottom={2}>
+                                            <Skeleton variant="rectangular" height={"400px"} width="300px" />
+                                        </Grid>
+                                        <Grid item xs={12} sm={6} lg={4} xl={3} marginBottom={2}>
+                                            <Skeleton variant="rectangular" height={"400px"} width="300px" />
+                                        </Grid>
+                                    </>
+                                )
                             }
                         </Grid>
                     </Box>
